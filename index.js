d25689363f02d3c0b7ec77531b00280a7be9341f
Dinkonad <naddinko@gmail.com>
2026-03-13 15:04:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import authRouter from './src/routes/auth.js';
 const app = express();
 app.use(cors());
 app.use(cors({
-  origin: ['https://tvoja-stranica.netlify.app', 'http://localhost:5173']
+  origin: ['https://inspector-teretana.netlify.app', 'http://localhost:5173']
 }));
 const PORT = process.env.PORT || 3000;
 
